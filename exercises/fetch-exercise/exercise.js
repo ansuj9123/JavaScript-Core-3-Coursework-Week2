fad109be1abd6bb0be1c2059430721df5129dbb5
Ansu Jammeh <ansuj9@yahoo.com>
2022-10-17 18:56:42 +0100

    Js3-week2 completed

--- exercises/fetch-exercise/exercise.js
+++ exercises/fetch-exercise/exercise.js
@@ -2,7 +2,7 @@
 Use fetch to load a greeting from the API and display it 
 in the HTML element with the id "greeting-text".
 
-API: https://codeyourfuture.herokuapp.com/api/greetings
+API endpoint: https://codeyourfuture.herokuapp.com/api/greetings
 Response: A greeting in a random language
 
 To learn more about fetch, refer to the doc:
@@ -16,11 +16,15 @@ Expected result
 Open index.html in your browser. Every time you refresh the page,
 a different greeting should be displayed in the box.
 */
+const paragraph = document.getElementById("greeting-text");
+fetch("https://codeyourfuture.herokuapp.com/api/greetings")
+ .then((response) => {
+  return response.text()
+ })
+ .then((greeting) => {
+  
+  paragraph.innerHTML = greeting
+ })
+ 
 
-fetch("*** Write the API address here ***")
-  .then(function (response) {
-    return response.text();
-  })
-  .then(function (greeting) {
-    // Write the code to display the greeting text here
-  });
+
